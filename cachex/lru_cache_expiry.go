9ee373153a3359ec7c3f8beb544b55cf3fe40ed7
Yangyu_wang <yangyu_wang@ntucenterprise.sg>
2022-11-06 00:48:08 +0800

    add lru cache with expiry

---
+++ cachex/lru_cache_expiry.go
@@ -0,0 +1,78 @@
+package cachex
+
+import (
+	"time"
+
+	"github.com/urpent/go/linklist"
+)
+
+type Options struct {
+	MaxTimeToLive time.Duration
+}
+
+type ValueWithExpiry[V any] struct {
+	Value     V
+	KeyExpiry time.Time
+}
+
+// LRUCacheExpiry is Least Recently Used (LRU) cache.
+type LRUCacheExpiry[K, V any] struct {
+	maxTimeToLive time.Duration
+	LRUCache[K, ValueWithExpiry[V]]
+}
+
+func NewLRUCacheExpiry[K, V any](capacity int, options Options) *LRUCacheExpiry[K, V] {
+	return &LRUCacheExpiry[K, V]{
+		maxTimeToLive: options.MaxTimeToLive,
+		LRUCache: LRUCache[K, ValueWithExpiry[V]]{
+			capacity: capacity,
+			cacheMap: make(map[any]*linklist.Node[keyValuePair[K, ValueWithExpiry[V]]], capacity+1),
+		},
+	}
+}
+
+func (c *LRUCacheExpiry[K, V]) Set(key K, value V) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
+	item, existed := c.cacheMap[key]
+	if existed {
+		c.linkedList.MoveNodeToFront(item)
+		return
+	}
+
+	valueWithExpiry := ValueWithExpiry[V]{
+		KeyExpiry: time.Now().Add(c.maxTimeToLive),
+		Value:     value,
+	}
+
+	node := c.linkedList.AddFront(keyValuePair[K, ValueWithExpiry[V]]{Key: key, Value: valueWithExpiry})
+	c.cacheMap[key] = node
+
+	// If max capacity reached
+	if len(c.cacheMap) > c.capacity {
+		node := c.linkedList.RemoveLast()
+		delete(c.cacheMap, node.Data.Key)
+	}
+}
+
+func (c *LRUCacheExpiry[K, V]) Get(key K) (value V, ok bool) {
+	c.mux.Lock()
+	defer c.mux.Unlock()
+
+	var item *linklist.Node[keyValuePair[K, ValueWithExpiry[V]]]
+	item, ok = c.cacheMap[key]
+	if !ok {
+		return
+	}
+
+	// if key expired
+	if time.Now().After(item.Data.Value.KeyExpiry) {
+		ok = false
+		return
+	}
+
+	c.linkedList.MoveNodeToFront(item)
+
+	return item.Data.Value.Value, true
+}
---
+++ cachex/lru_cache_expiry_test.go
@@ -0,0 +1,56 @@
+package cachex
+
+import (
+	"testing"
+	"time"
+
+	"github.com/urpent/go/ut"
+)
+
+func Test_LRUCacheExpiry(t *testing.T) {
+	t.Run("test", func(t *testing.T) {
+		cache := NewLRUCacheExpiry[int, int](2, Options{MaxTimeToLive: 1 * time.Minute})
+
+		cache.Set(1, 1)
+		cache.Set(2, 2) // cache is full after set 2
+		ut.AssertEqual(t, len(cache.cacheMap), 2)
+
+		result, _ := cache.Get(1)
+		ut.AssertEqual(t, 1, result) // ok return 1
+		cache.Set(3, 3)              // LRU key was 2, evicts key 2, cache is {3=3, 1=1}
+		_, ok := cache.Get(2)        // 2 not found
+		ut.AssertEqual(t, false, ok)
+		ut.AssertEqual(t, len(cache.cacheMap), 2)
+
+		// now cache is {3=3, 1=1}, 1 is LRU
+		cache.Set(1, 1) // 3 become LRU {1=1, 3=3}
+
+		cache.Set(4, 4) // LRU key was 1, evicts key 1, cache is {4=4, 1=1}
+		_, ok = cache.Get(3)
+		ut.AssertEqual(t, false, ok)
+
+		result, _ = cache.Get(1)
+		ut.AssertEqual(t, 1, result)
+
+		result, _ = cache.Get(4)
+		ut.AssertEqual(t, 4, result)
+
+		ut.AssertEqual(t, len(cache.cacheMap), 2)
+		ut.AssertEqual(t, cache.linkedList.Len(), 2)
+	})
+}
+
+func Test_LRUCacheExpiry_Expired(t *testing.T) {
+	t.Run("test cache expiry", func(t *testing.T) {
+		cache := NewLRUCacheExpiry[int, int](3, Options{MaxTimeToLive: 30 * time.Millisecond})
+
+		cache.Set(1, 1)
+		cache.Set(2, 2)
+		result, _ := cache.Get(1)
+		ut.AssertEqual(t, 1, result) // ok, key 1 not expired yet, return 1
+
+		time.Sleep(30 * time.Millisecond)
+		_, ok := cache.Get(2)
+		ut.AssertEqual(t, false, ok) // 2 is expired and should not be found
+	})
+}
