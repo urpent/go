07ac6c344cf2e911a9d0c270db7fc36672d729e8
Yangyu_wang <yangyu_wang@ntucenterprise.sg>
2022-11-20 23:24:02 +0800

    new api structure

--- api/api.go
+++ api/api.go
@@ -5,26 +5,36 @@ import (
 	"net/http"
 )
 
-type Resp[T any] struct {
-	StatusCode int `json:"statusCode"`
-	Data       T   `json:"data,omitempty"`
+type Status string
+
+const (
+	StatusOk           = "OK"
+	StatusNotFound     = "NOT_FOUND"
+	StatusBadRequest   = "BAD_REQUEST"
+	StatusUnauthorized = "UNAUTHORIZED"
+	StatusServerError  = "SERVER_ERROR"
+)
+
+type Response[T any] struct {
+	Status  Status `json:"status"`
+	Message string `json:"message,omitempty"`
+	Data    T      `json:"data,omitempty"`
 }
 
-type RespList[T any] struct {
-	Resp[List[T]]
-}
-
+// List is used by query api in repo & service layer.
+// Some clients require total records for pagination.
+// Therefore, there will be an optional total field.
 type List[T any] struct {
-	Total  int  `json:"total"`
+	Total  *int `json:"total,omitempty"`
 	Limit  *int `json:"limit,omitempty"`
 	Offset *int `json:"offset,omitempty"`
 	List   []T  `json:"list"`
 }
 
-func Json[T any](w http.ResponseWriter, statusCode int, data T) {
+func Json[T any](w http.ResponseWriter, status Status, data T) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(Resp[T]{
-		StatusCode: statusCode,
-		Data:       data,
+	json.NewEncoder(w).Encode(Response[T]{
+		Status: status,
+		Data:   data,
 	}) //#nosec
 }
