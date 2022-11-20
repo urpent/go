4867062d54151f2c699be08f685500bd0d1cc420
Yangyu_wang <yangyu_wang@ntucenterprise.sg>
2022-11-20 21:33:25 +0800

    add api list response

--- api/api.go
+++ api/api.go
@@ -10,6 +10,13 @@ type Resp[T any] struct {
 	Data       T   `json:"data,omitempty"`
 }
 
+type List[T any] struct {
+	Total  int  `json:"total"`
+	Limit  *int `json:"limit,omitempty"`
+	Offset *int `json:"offset,omitempty"`
+	List   []T  `json:"list"`
+}
+
 func Json[T any](w http.ResponseWriter, statusCode int, data T) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(Resp[T]{
